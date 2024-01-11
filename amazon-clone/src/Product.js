fb7845ee15c1d2173ed5ffa8fc48ac9092252697
Atul V Chandran <atulvc2001@gmail.com>
2024-01-11 17:20:18 +0530

    update: modified the home page, added multiple products

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import "./Product.css"
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className='product'>
         <div className="product__info">
-            <p>The lean startup</p>
+            <p>{title}</p>
             <p className="product__price">
-                <small>S</small>
-                <strong>19.99</strong>
+                <small>$</small>
+                <strong>{price}</strong>
             </p>
+          <div className="product__rating">
+              {Array(rating).fill().map((_,i)=>(
+                <p>⭐</p>
+              ))}
+          </div>
         </div>
-            <div className="product__rating">
-                <p>⭐</p>
-                <p>⭐</p>
-                <p>⭐</p>
-            </div>
 
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg" alt="prod_img" />
+        <img src={image} alt="prod_img" />
         
-            <button>Add to Basket</button>
+        <button>Add to Basket</button>
     </div>
   )
 }
