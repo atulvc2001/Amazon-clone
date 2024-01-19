57ec8fdb5514729c4e2b45dfdc029d1c1e6c3a77
Atul V Chandran <atulvc2001@gmail.com>
2024-01-20 01:42:14 +0530

    feat: username will be shown in the header when signed in

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -3,9 +3,12 @@ import "./Subtotal.css"
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from './StateProvider';
 import { getBasketTotal } from './reducer';
+import { useNavigate } from 'react-router-dom';
 
 function Subtotal() {
+  
   const [{ basket },dispatch] = useStateValue();
+  const navigate = useNavigate();
   // console.log(basket)
 
   return (
@@ -31,7 +34,7 @@ function Subtotal() {
       prefix={"$"}
       />
 
-      <button>Proceed to Checkout</button>
+      <button onClick={()=>navigate("/payment")}>Proceed to Checkout</button>
     </div>
   )
 }
